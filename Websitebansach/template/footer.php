d9dd0ac3988053882199bb8b9eb56a17a1e603f1
khoihuynh12 <91311007+khoihuynh12@users.noreply.github.com>
2021-12-03 16:18:26 +0700

    Update footer.php

--- Websitebansach/template/footer.php
+++ Websitebansach/template/footer.php
@@ -2,7 +2,6 @@
 
       	<footer>
         	<div class="text-muted pull-left">
-            	<a href="http://projectworlds.in" target="_blank"> projectworlds </a>
         	</div>
         	<div class="text-muted pull-right">
           		<a href="admin.php">Admin Login</a> 2021
