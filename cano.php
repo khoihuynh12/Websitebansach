ea48aeae08883ac7db596cd8d9eb5cdb97f58c68
nguyenngocctri <85422453+nguyenngocctri@users.noreply.github.com>
2021-12-29 11:42:36 +0700

    Add files via upload

---
+++ cano.php
@@ -0,0 +1,21 @@
+<style>
+    body {
+  background-color:powderblue;
+}
+    
+
+p1{
+    height: 1000px;
+   width: 100%;
+
+}
+button{
+    color:black;
+}
+
+    </style>
+    <body>
+<center><p1>CẢM ƠN QUÝ KHÁCH ĐÃ MUA HÀNG</p1><center>
+<br><br>
+<button><a href="index.php">QUAY VỀ TRANG CHỦ </a></button>
+</body>
